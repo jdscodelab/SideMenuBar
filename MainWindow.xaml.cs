3a9cb8e06736394fd25c4c957858e03f9d7c6eb5
Jd's Code Lab <jdscodelab@gmail.com>
2020-07-27 10:07:30 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SideMenuListControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //this is to set default page on application startup such as dashboard...etc..
+            frame.Navigate(new Uri("Page1.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void SideMenuControl_SelectionChanged(object sender, EventArgs e)
+        {
+            switch (MenuList.SelectedIndex)
+            {
+                case 0:
+                    frame.Navigate(new Uri("Page1.xaml", UriKind.RelativeOrAbsolute));
+                    break;
+                case 1:
+                    frame.Navigate(new Uri("Page2.xaml", UriKind.RelativeOrAbsolute));
+                    break;
+            }
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SideMenuListControl
+{
+    class ViewModel
+    {
+        //To call resource dictionary in our codebehind
+        ResourceDictionary dict = Application.LoadComponent(new Uri("/SideMenuListControl;component/IconDictionary.xaml", UriKind.RelativeOrAbsolute)) as ResourceDictionary;
+
+        //Source list for our Menu Items Listbox
+        public List<MenuItemsData> ItemsList
+        {
+            get {
+                return new List<MenuItemsData> { 
+                new MenuItemsData(){ PathData= (PathGeometry)dict["HomeIcon"], MenuText="Home" },
+                new MenuItemsData(){ PathData = (PathGeometry)dict["MailIcon"], MenuText="Mail" } };
+            }}
+    }
+
+    public class MenuItemsData {
+        public PathGeometry PathData { get; set; }
+        public bool IsItemSelected { get; set; }
+        public string MenuText { get; set; }
+    }
+}
