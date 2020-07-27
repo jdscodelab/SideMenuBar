4c5cdcbf65dd7a735a64134acbac0520f4955f6b
Jd's Code Lab <jdscodelab@gmail.com>
2020-07-27 10:35:23 +0530

    Add files via upload

---
+++ Custom Controls/SideMenuControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SideMenuListControl.Custom_Controls
+{
+    /// <summary>
+    /// Interaction logic for SideMenuControl.xaml
+    /// </summary>
+    public partial class SideMenuControl : UserControl
+    {
+        public SideMenuControl()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel();
+        }
+
+        public int SelectedIndex
+        {
+            get { return (int)GetValue(SelectedIndexProperty); }
+            set { SetValue(SelectedIndexProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedIndex.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedIndexProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(int), typeof(SideMenuControl));
+
+
+        //Since we want to navigate through different pages in listbox item selection change event we are declaring  an event in our usercontrol for that...
+        public event EventHandler SelectionChanged;
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectionChanged?.Invoke(sender, e);
+
+            //That was simple.. wasnt it?
+        }
+    }
+}
